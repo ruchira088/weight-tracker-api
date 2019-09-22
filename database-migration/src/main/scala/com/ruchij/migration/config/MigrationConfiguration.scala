bbd9a398805dd7c8cd9e715f75f639c348c05e51
Ruchira (Richie) Jayasekara <me@ruchij.com>
2019-09-22 18:25:43 +1000

    Adding Redis to the Docker Compose yml

--- database-migration/src/main/scala/com/ruchij/migration/config/MigrationConfiguration.scala
+++ database-migration/src/main/scala/com/ruchij/migration/config/MigrationConfiguration.scala
@@ -1,12 +1,12 @@
 package com.ruchij.migration.config
 
 import pureconfig.error.ConfigReaderException
-import pureconfig.loadConfig
+import pureconfig.ConfigSource
 import pureconfig.generic.auto._
 
 case class MigrationConfiguration(databaseConfiguration: DatabaseConfiguration)
 
 object MigrationConfiguration {
   def load(): Either[Exception, MigrationConfiguration] =
-    loadConfig[MigrationConfiguration].left.map(ConfigReaderException.apply)
+    ConfigSource.default.load[MigrationConfiguration].left.map(ConfigReaderException.apply)
 }

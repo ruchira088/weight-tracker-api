5be13f68d3a0934b487d06f077ed6e37c4d8e6c8
Ruchira (Richie) Jayasekara <me@ruchij.com>
2019-10-12 00:17:22 +1100

    Refactoring

--- src/main/scala/com/ruchij/services/authorization/Permission.scala
+++ src/main/scala/com/ruchij/services/authorization/Permission.scala
@@ -5,18 +5,18 @@ import enumeratum.{Enum, EnumEntry}
 import scala.collection.immutable.IndexedSeq
 
 sealed trait Permission extends EnumEntry {
-  val containsPermissions: Set[Permission] = Set.empty
+  val includedPermissions: Set[Permission] = Set.empty
 }
 
 object Permission extends Enum[Permission] {
   case object READ extends Permission
 
   case object WRITE extends Permission {
-    override val containsPermissions: Set[Permission] = Set(READ)
+    override val includedPermissions: Set[Permission] = Set(READ)
   }
 
   override def values: IndexedSeq[Permission] = findValues
 
   def allPermissions(permission: Permission): Set[Permission] =
-    permission.containsPermissions.flatMap(allPermissions) + permission
+    permission.includedPermissions.flatMap(allPermissions) + permission
 }

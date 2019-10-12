e603e4535373ecebec3281f0a4ec8af4b2d07681
Ruchira (Richie) Jayasekara <me@ruchij.com>
2019-10-12 16:16:59 +1100

    Added description to the Transformation type class

--- src/main/scala/com/ruchij/types/Transformation.scala
+++ src/main/scala/com/ruchij/types/Transformation.scala
@@ -7,14 +7,18 @@ import com.ruchij.exceptions.AggregatedException
 import scala.concurrent.Future
 import scala.language.higherKinds
 
+/**
+ * This type class differs with [[cats.arrow.FunctionK]] ([[cats.~>]]) by the argument to the apply function being
+ * call-by-name rather then call-by-value.
+ */
 trait Transformation[F[_], G[_]] {
   def apply[A](value: => F[A]): G[A]
 }
 
 object Transformation {
-  def apply[F[_], G[_]](implicit transformation: F ~> G): F ~> G = transformation
-
   type ~>[F[_], G[_]] = Transformation[F, G]
+
+  def apply[F[_], G[_]](implicit transformation: F ~> G): F ~> G = transformation
 
   implicit def futureToIo(implicit contextShift: ContextShift[IO]): Future ~> IO =
     new Transformation[Future, IO] {
